8ce0eadfc94d0f6c39995c29756856b0e3be4f62
David Hörl <hoerldavid@gmail.com>
2017-12-14 17:32:30 +0100

    added some logging

--- simple_detection.py
+++ simple_detection.py
@@ -18,9 +18,13 @@ import matplotlib.patches as mpatches
 from skimage.transform import rescale, pyramid_gaussian, resize
 from skimage.color import label2rgb
 
+import logging
+
 def bbox_pix2unit(bbox, start, pixsize, direction):
+    logger = logging.getLogger(__name__)
     res = (np.array(bbox, dtype=float).reshape((2,2)) * np.array(pixsize, dtype=float) *
             np.array(direction, dtype=float) + np.array(start, dtype=float))
+    logger.debug('bbox: {}, toUnit: {}'.format(bbox, res.reshape((4,))))
     return res.reshape((4,))
 
 def aspect(bbox):
@@ -33,10 +37,21 @@ def detect_wings_simple(img, start, pixsize, direction,
                         ds=2, layers=2, thresh_window=351,
                         minarea=20000, maxarea=80000, minsolidity=.6,
                         minaspect=.3, plot=False, threshold_fun=None):
+
+    logger = logging.getLogger(__name__)
+    # some debug output:
+    logger.info('wing detection started')
+    logger.debug('detection start: {}'.format(start))
+    logger.debug('detection pixsize: {}'.format(pixsize))
+    logger.debug('detection direction: {}'.format(direction))
+    logger.debug('input shape: {}'.format(img.shape))
+    logger.debug('ds: {}, layer:{}'.format(ds, layers))
+
     # downsample
     pyr = [p for p in pyramid_gaussian(img, max_layer= layers, downscale = ds)]
     img_ds = pyr[layers]
 
+    logger.debug('img size after ds: {}'.format(img_ds.shape))
     # rescale to (0-1)
     img_ds = img_ds.astype(float)
     rescale_intensity(img_ds, out_range=(0.0, 1.0))
@@ -50,8 +65,10 @@ def detect_wings_simple(img, start, pixsize, direction,
     thrd = np.bitwise_not(thrd)
     thrd = binary_opening(thrd, selem=disk(4))
 
+    labelled = label(thrd)
+
     # filter objs
-    ls = [r.label for r in regionprops(label(thrd)) if r.area>minarea and
+    ls = [r.label for r in regionprops(labelled) if r.area>minarea and
           r.area<maxarea and r.solidity>minsolidity and aspect(r.bbox) > minaspect]
     
     # filtered binary
@@ -75,14 +92,17 @@ def detect_wings_simple(img, start, pixsize, direction,
     for r in regionprops(label(r2)):
         if r.area < (minarea * .8 ):
             continue
-            
-        bboxes.append(np.array(r.bbox) * (ds**layers))
+
+        bbox_scaled = np.array(r.bbox) * (ds**layers)
+        logger.debug('bbox: {}, upsampled: {}'.format(r.bbox, bbox_scaled))
+        bboxes.append(bbox_scaled)
         if plot:
             minr, minc, maxr, maxc = r.bbox
             rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                   fill=False, edgecolor='red', linewidth=2)
             ax.add_patch(rect)
-    
+
+    logger.info('found {} object(s)'.format(len(bboxes)) )
     # pixels to units
     return [bbox_pix2unit(b, start, pixsize, direction) for b in bboxes]
 
